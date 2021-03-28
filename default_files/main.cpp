0871103feeeb299ea069d0ddb2bf1721108a95e6
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2021-03-28 15:57:41 -0300

    initial commit

---
+++ default_files/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define int ll
+#define fori(x, n) for(int i=x; i<int(n); i++)
+
+#define cins(s) string s; cin>>s;
+#define cini(i) int i; cin>>i;
+#define cind(d) double d; cin>>d;
+#define cinai(a, n) vi a(n); fori(0, n) { cin>>a[i]; }
+#define cinas(s, n) vs s(n); fori(0, n) { cin>>s[i]; }
+#define cinad(a, n) vd a(n); fori(0, n) { cin>>a[i]; }
+
+typedef vector<double> vd;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+
+
+void solve() {
+
+	return;
+}
+
+int main(){
+
+	cini(t);
+
+	while (t--) {
+		solve();
+	}
+}
