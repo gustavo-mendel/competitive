de34d888963022f36d703a5347c1227bbd20419b
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2021-04-18 19:37:24 -0300

    algumas coisas apenas

---
+++ kickstart/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+// By: mende1
+
+#define endl "\n"
+#define int ll
+#define cini(n) int n; cin >> n;
+#define fori(x, n) for(int i=x; i<=n; i++)
+#define forj(x, n) for(int j=x; j<=n; j++)
+#define fork(x, n) for(int k=x; k<=n; k++)
+
+#define N 1'000'000'000'000L
+
+void precal () {
+	return;
+}
+
+void solve() {
+	// ----------------------------
+
+	
+
+	// ----------------------------
+}
+
+int32_t main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	precal();
+	cini(t);
+	// ll t=1;
+
+	while(t--) {
+		solve();
+	}
+}
